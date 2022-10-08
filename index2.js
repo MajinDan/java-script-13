d74a1b2874aaf4970b2baeb610f1fac92b17b7d3
MajinDan <95249871+MajinDan@users.noreply.github.com>
2022-10-08 13:24:36 +0100

    finshed lesson

---
+++ index2.js
@@ -0,0 +1,10 @@
+//write consise anonymous functions
+
+// var magic = () => new Date();
+// console.log(magic)
+
+//write arrows functions with parameters
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console(myConcat([1,2],[3,4,5]));
---
+++ index4.js
@@ -0,0 +1,9 @@
+//default parameters
+const increment = (function() {
+    return function increment(number, value = 1) {
+        return number + value;
+    };
+})()
+
+console.log(increment(5, 2));
+console.log(increment(5));
